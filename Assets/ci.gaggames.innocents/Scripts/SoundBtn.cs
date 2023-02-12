3db928b77737c5b5f58ff9ac252f94fc7092701c
SGCEntertainment <nikcxprojects@gmail.com>
2023-02-12 14:22:41 +0300

    update

---
+++ Assets/ci.gaggames.innocents/Scripts/SoundBtn.cs
@@ -0,0 +1,23 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SoundBtn : MonoBehaviour
+{
+    private Text Text { get; set; }
+    private Button Button { get; set; }
+
+    private void Awake()
+    {
+        Button = GetComponent<Button>();
+        Text = Button.GetComponentInChildren<Text>();
+    }
+
+    private void Start()
+    {
+        Button.onClick.AddListener(() =>
+        {
+            AudioListener.pause = !AudioListener.pause;
+            Text.text = $"Sound: {(AudioListener.pause ? "off" : "on")}";
+        });
+    }
+}
